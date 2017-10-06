2f4701b35aa8f22d480442dba7611715b96b9915
Manroop Singh <singh@manroop.com>
2017-10-06 16:22:49 -0400

    more images in level list

--- Samples/android-LevelListDrawable/app/src/main/java/com/example/user/android_levellistdrawable/MainActivity.java
+++ Samples/android-LevelListDrawable/app/src/main/java/com/example/user/android_levellistdrawable/MainActivity.java
@@ -3,19 +3,48 @@ package com.example.user.android_levellistdrawable;
 import android.graphics.drawable.LevelListDrawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.ImageView;
 
+import java.util.Random;
+
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivityTag";
+    private ImageView imageView;
+    private LevelListDrawable levelListDrawable;
+    public static final int IMAGE_LEVELS = 5;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ImageView imageView = (ImageView) findViewById(R.id.imageView);
-        LevelListDrawable levelListDrawable = (LevelListDrawable) imageView.getBackground();
-        levelListDrawable.setLevel(1);
+        imageView = (ImageView) findViewById(R.id.imageView);
+        levelListDrawable = (LevelListDrawable) imageView.getBackground();
+
         //imageView.setImageLevel(1);
 
+    }
+
+    public void changeImage(View view) {
+
+
+        int randomLevel = getRandomLevel();
+        int currentLevel = levelListDrawable.getLevel();
+
+        Log.d(TAG, "changeImage: Current:" + currentLevel);
+        Log.d(TAG, "changeImage: Random:" + randomLevel);
+        while(currentLevel == randomLevel){
+            randomLevel = getRandomLevel();
+        }
+
+        levelListDrawable.setLevel(randomLevel);
+
+    }
+
+    private int getRandomLevel() {
+        return new Random().nextInt(IMAGE_LEVELS);
     }
 }
