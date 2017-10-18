cc344b62a4e1afb29d08f0e95a9d1ed5c595708d
ManroopSingh <singh@manroop.com>
2017-10-17 22:27:45 -0400

    ble detail activity

---
+++ Samples/android-Bluetooth/app/src/main/java/com/example/singh/android_bluetooth/DeviceDetailActivity.java
@@ -0,0 +1,16 @@
+package com.example.singh.android_bluetooth;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DeviceDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_device_detail);
+
+
+        
+    }
+}
--- Samples/android-Bluetooth/app/src/main/java/com/example/singh/android_bluetooth/DeviceListActivity.java
+++ Samples/android-Bluetooth/app/src/main/java/com/example/singh/android_bluetooth/DeviceListActivity.java
@@ -1,5 +1,7 @@
 package com.example.singh.android_bluetooth;
 
+import android.Manifest;
+import android.app.AlertDialog;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
 import android.bluetooth.BluetoothManager;
@@ -9,15 +11,17 @@ import android.bluetooth.le.ScanFilter;
 import android.bluetooth.le.ScanResult;
 import android.bluetooth.le.ScanSettings;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
 
@@ -28,6 +32,7 @@ public class DeviceListActivity extends AppCompatActivity {
 
     private static final int REQUEST_ENABLE_BT = 10;
     private static final String TAG = "BluetoothActivityTag";
+    private static final int REQUEST_LOCATION = 20;
     private BluetoothAdapter mBluetoothAdapter;
     private boolean mScanning;
     private Handler mHandler = new Handler();
@@ -43,14 +48,87 @@ public class DeviceListActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bluetooth);
 
+//        Check if device support ble
         if (!getPackageManager().hasSystemFeature(PackageManager.FEATURE_BLUETOOTH_LE)) {
-            Log.d(TAG, "onCreate: ");
             Toast.makeText(this, "BLE is not supported", Toast.LENGTH_SHORT).show();
             finish();
         }
 
+//        Request location permission
+        requestLocationPermission();
 
-// Initializes Bluetooth adapter.
+
+//        Initializes Bluetooth adapter.
+        initializeBLE();
+
+//        setup recycler view for to list nearby devices
+        setupRecyclerView();
+
+    }
+
+    private void requestLocationPermission() {
+        if(ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED){
+
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                    REQUEST_LOCATION);
+
+        }
+    }
+
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+
+        switch (requestCode) {
+            case REQUEST_LOCATION: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                    // permission was granted, yay! Do the
+                    // contacts-related task you need to do.
+
+                } else {
+                    AlertDialog alertDialog = new AlertDialog.Builder(this)
+                            .setTitle("Allow location")
+                            .setMessage(R.string.location_permission_rationale)
+                            .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialog, int which) {
+                                    requestLocationPermission();
+                                }
+                            })
+                            .setNegativeButton("No", new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialog, int which) {
+                                    Toast.makeText(DeviceListActivity.this, "Location denied. Results might not be accurate", Toast.LENGTH_SHORT).show();
+                                }
+                            })
+                            .create();
+                    alertDialog.show();
+
+
+
+
+                }
+                return;
+
+
+
+            }
+
+            case REQUEST_ENABLE_BT:
+
+                break;
+
+        }
+
+
+    }
+
+    private void initializeBLE() {
         final BluetoothManager bluetoothManager =
                 (BluetoothManager) getSystemService(Context.BLUETOOTH_SERVICE);
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR2) {
@@ -71,14 +149,14 @@ public class DeviceListActivity extends AppCompatActivity {
             }
             scanLeDevice(true);
         }
+    }
 
+    private void setupRecyclerView() {
         RecyclerView recyclerView = (RecyclerView) findViewById(R.id.rvDeviceList);
         RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this);
         deviceListAdapter = new DeviceListAdapter();
         recyclerView.setLayoutManager(layoutManager);
         recyclerView.setAdapter(deviceListAdapter);
-
-
     }
 
     private void scanLeDevice(final boolean enable) {
