1497870390bf5fa79a26ef82f6adae463a7af0a2
Manroop Singh <singh@manroop.com>
2018-01-02 21:04:42 -0500

    codelab for android architecture components

---
+++ codelabs/codelab-AndroidArchitecureComponents/app/src/main/java/com/example/user/codelab_androidarchitecurecomponents/TestingObserver.java
@@ -0,0 +1,13 @@
+package com.example.user.codelab_androidarchitecurecomponents;
+
+import android.arch.lifecycle.LifecycleObserver;
+import android.arch.lifecycle.OnLifecycleEvent;
+
+/**
+ * Created by singh on 1/2/18.
+ */
+
+public class TestingObserver implements LifecycleObserver {
+
+
+}
