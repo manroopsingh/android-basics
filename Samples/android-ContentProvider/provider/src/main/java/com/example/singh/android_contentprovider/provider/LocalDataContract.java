6d7f0ce519a43b71b334283f3d554339dac7b72c
ManroopSingh <singh@manroop.com>
2018-01-03 23:50:46 -0500

    update provider

--- Samples/android-ContentProvider/provider/src/main/java/com/example/singh/android_contentprovider/provider/LocalDataContract.java
+++ Samples/android-ContentProvider/provider/src/main/java/com/example/singh/android_contentprovider/provider/LocalDataContract.java
@@ -11,6 +11,6 @@ public final class LocalDataContract {
         public static final String NAME = "name";
         public static final String AGE = "age";
         public static final String GENDER = "gender";
-        
+
     }
 }
