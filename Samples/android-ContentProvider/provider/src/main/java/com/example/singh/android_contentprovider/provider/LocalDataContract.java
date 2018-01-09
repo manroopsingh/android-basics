c158459b506d32a744f6eef7ef36383a1d952618
ManroopSingh <singh@manroop.com>
2018-01-08 22:57:27 -0500

    content provider update

--- Samples/android-ContentProvider/provider/src/main/java/com/example/singh/android_contentprovider/provider/LocalDataContract.java
+++ Samples/android-ContentProvider/provider/src/main/java/com/example/singh/android_contentprovider/provider/LocalDataContract.java
@@ -16,11 +16,9 @@ public final class LocalDataContract {
         public static final String AGE = "age";
         public static final String GENDER = "gender";
 
-<<<<<<< HEAD
-=======
         //table DDL statements
         public static final String CREATE_TABLE = "CREATE TABLE " + TABLE_NAME +
                 " (" + NAME;
->>>>>>> 72946e1a780e96713249a356232fd7ef39f6e0b6
+
     }
 }
