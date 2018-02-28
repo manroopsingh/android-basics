f65011958d216c407e017afcd672d2cc9a60aa02
ManroopSingh <singh@manroop.com>
2018-02-27 22:40:47 -0500

    update content provider

---
+++ Samples/android-ContentProvider/provider/src/main/java/com/example/singh/android_contentprovider/MainCursorProvider.java
@@ -0,0 +1,27 @@
+package com.example.singh.android_contentprovider;
+
+import android.content.Context;
+import android.database.Cursor;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.CursorAdapter;
+
+/**
+ * Author: singh on: 19-Feb-18.
+ */
+
+public class MainCursorProvider extends CursorAdapter {
+    public MainCursorProvider(Context context, Cursor c, boolean autoRequery) {
+        super(context, c, autoRequery);
+    }
+
+    @Override
+    public View newView(Context context, Cursor cursor, ViewGroup parent) {
+        return null;
+    }
+
+    @Override
+    public void bindView(View view, Context context, Cursor cursor) {
+
+    }
+}
