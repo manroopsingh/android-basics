158dbba0194b5632755e0f1cf34b756e121c405c
Manroop Singh <singh@manroop.com>
2018-07-14 12:54:52 -0400

    added expression calculator

--- Java-Basics/java-InterviewCoding/myapplication/src/main/java/com/example/myapplication/MainActivity.java
+++ Java-Basics/java-InterviewCoding/myapplication/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,14 +1,63 @@
 package com.example.myapplication;
 
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.app.TaskStackBuilder;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Build;
+import android.support.v4.app.NotificationCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    private NotificationChannel channel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+    }
+
+
+    public void onNotifForA(View view) {
+        sendNotification(FirstActivity.class, "Send for First", "Some content");
+    }
+
+    public void onNotifForB(View view) {
+        sendNotification(SecondActivity.class, "Send for Second", "Some content");
+    }
+
+    public void sendNotification(Class c, String title, String content) {
+
+        String channelId = "channelId";
+        String channelName = "channelName";
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            channel = new NotificationChannel(channelId, channelName, NotificationManager.IMPORTANCE_HIGH);
+        }
+
+        Intent intent = new Intent(this, c);
+        TaskStackBuilder taskStackBuilder = TaskStackBuilder.create(this);
+        taskStackBuilder.addNextIntentWithParentStack(intent);
+        PendingIntent pendingIntent = taskStackBuilder.getPendingIntent(0, PendingIntent.FLAG_UPDATE_CURRENT);
+
+        NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            notificationManager.createNotificationChannel(channel);
+        }
+
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this, channelId)
+                .setSmallIcon(android.R.drawable.ic_dialog_alert)
+                .setContentTitle(title)
+                .setContentText(content)
+                .setContentIntent(pendingIntent);
+
+        notificationManager.notify(0, builder.build());
 
 
     }
